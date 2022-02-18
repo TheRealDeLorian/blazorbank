4f415e4cd28d236effd78582ce59b68654afe8e8
Jonathan Allen <jallen@snow.edu>
2022-02-18 09:39:42 -0700

    Load both checking and savings accounts

--- Pages/Account.cs
+++ Pages/Account.cs
@@ -8,7 +8,7 @@ public abstract class Account
     }
 
     public string Name { get; }
-    public decimal Balance{get;private set;}
+    public decimal Balance { get; private set; }
 
     public virtual void MakeDeposit(decimal depositAmount)
     {
@@ -17,8 +17,46 @@ public abstract class Account
 
     public virtual void Save(StreamWriter writer)
     {
-        writer.WriteLine("Name:"+this.Name);
-        writer.WriteLine("Balance:"+this.Balance);
+        writer.WriteLine("Name:" + this.Name);
+        writer.WriteLine("Balance:" + this.Balance);
+    }
+
+    public static List<Account> Load(string path)
+    {
+        var accounts = new List<Account>();
+        var accountName = "";
+        var balance = 0M;
+        CheckingAccountLevel accountLevel = CheckingAccountLevel.Standard;
+        foreach (var line in File.ReadAllLines(path))
+        {
+            var parts = line.Split(':');
+            if (parts[0] == "Name")
+            {
+                accountName = parts[1];
+            }
+            else if (parts[0] == "Balance")
+            {
+                balance = decimal.Parse(parts[1]);
+            }
+            else if (parts[0] == "AccountLevel")
+            {
+                accountLevel = (CheckingAccountLevel)Enum.Parse(typeof(CheckingAccountLevel), parts[1]);
+            }
+            else if (parts[0] == "AccountType")
+            {
+                switch (parts[1])
+                {
+                    case "SavingsAccount":
+                        accounts.Add(new SavingsAccount(accountName));
+                        break;
+                    case "CheckingAccount":
+                        accounts.Add(new CheckingAccount(accountName, accountLevel));
+                        break;
+                }
+            }
+        }
+
+        return accounts;
     }
 }
 
@@ -29,12 +67,13 @@ public class SavingsAccount : Account
 
     }
 
-    public decimal MaxBalance{get;set;}
+    public decimal MaxBalance { get; set; }
 
     public override void Save(StreamWriter writer)
     {
         base.Save(writer);
-        writer.WriteLine("MaxBalance:"+MaxBalance);
+        writer.WriteLine("MaxBalance:" + MaxBalance);
+        writer.WriteLine("AccountType:SavingsAccount");
     }
 }
 
@@ -50,7 +89,8 @@ public class CheckingAccount : Account
     public override void Save(StreamWriter writer)
     {
         base.Save(writer);
-        writer.WriteLine("AccountLevel:"+AccountLevel);
+        writer.WriteLine("AccountLevel:" + AccountLevel);
+        writer.WriteLine("AccountType:CheckingAccount");
     }
 }
 
